ec30d3d69b1a01cded966925a816ddb66fcda6b5
Jawad Ahmed <soomrojawad1.2.2006@gmail.com>
2024-10-31 07:21:27 +0500

    added q6

--- LAB TASK#8/q6.c
+++ LAB TASK#8/q6.c
@@ -1,7 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-     
- 
- return 0;
+    // int coficient = 1;
+
+    int arr[6][6] = {0};
+    for (int i = 0; i < 6; i++)
+    {
+
+        arr[i][0] = 1;
+        for (int j = 1; j < i; j++)
+        {
+            arr[i][j] = (arr[i - 1][j - 1] + arr[i - 1][j]);
+        }
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        for (int n = 6; n > i; n--)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            printf(" %d", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
