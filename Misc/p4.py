243c0542bd102b4a97b186c43ea65f5883bae86e
Jawad Ahmed <soomrojawad1.2.2006@gmail.com>
2024-12-15 08:25:12 +0500

    Update p4.py

--- Misc/p4.py
+++ Misc/p4.py
@@ -1,7 +1,7 @@
 import math
 print("Projectile Motion")
-Vi=float(input("Enter initial velocity"))
-x=float(input("Enter launch angle of the projectile"))
+Vi=float(input("Enter initial velocity (m/s) "))
+x=float(input("Enter launch angle of the projectile (degree) "))
 
 H=((Vi**2)*(math.sin(math.radians(x)))**2)/(2*9.8);
 
@@ -10,6 +10,6 @@ R=((Vi**2)*(math.sin(math.radians(2*x))))/9.8;
 T=(2*(Vi**2)*(math.sin(math.radians(x))))/9.8;
 
 
-print("Maximum Height reached : {H}m")
-print("Range of projectile : {R}m")
-print("Total time of flight : {T}s")
+print(f"Maximum Height reached : {H}m")
+print(f"Range of projectile : {R}m")
+print(f"Total time of flight : {T}s")
