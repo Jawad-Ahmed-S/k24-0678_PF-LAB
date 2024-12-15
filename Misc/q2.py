be30db57666b217dfe443c1c78a216afdb3e0431
Jawad Ahmed <soomrojawad1.2.2006@gmail.com>
2024-12-15 08:31:05 +0500

    Update q2.py

--- Misc/q2.py
+++ Misc/q2.py
@@ -3,4 +3,4 @@ q1=float(input("Enter the charge 1 (in Coloumbs) : "))
 q2=float(input("Enter the charge 2 (in Coloumbs) : "))
 r=float(input("Enter the sepration(r) (in meters) : "))
 Force = ((9*(10**9))*(q1)*(q2))/r**2
-print("{Force} N")
+print(f"{Force} N")
